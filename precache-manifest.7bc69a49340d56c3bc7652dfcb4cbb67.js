b35aa2b3906861d142994a04d736847e39c55820
Yeskina <angelina.yeskina@gmail.com>
2020-09-01 14:26:04 +0300

    Updates

---
+++ precache-manifest.7bc69a49340d56c3bc7652dfcb4cbb67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b673bd65709314d32c213ab6eeca8fa9",
+    "url": "/credit-card/index.html"
+  },
+  {
+    "revision": "2a02743885248a5a44b9",
+    "url": "/credit-card/static/css/main.96ccb57b.chunk.css"
+  },
+  {
+    "revision": "3e390208086a4d564eab",
+    "url": "/credit-card/static/js/2.e6fdd36e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/credit-card/static/js/2.e6fdd36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a02743885248a5a44b9",
+    "url": "/credit-card/static/js/main.6db8182e.chunk.js"
+  },
+  {
+    "revision": "702d76c017e5d3a66bb1",
+    "url": "/credit-card/static/js/runtime-main.6c2bab88.js"
+  },
+  {
+    "revision": "30a01e292048564f8b7ae3e5c9eb62b7",
+    "url": "/credit-card/static/media/54856614-abstract-vector-background-for-use-in-design.30a01e29.jpg"
+  },
+  {
+    "revision": "4ed75f15494b54507601e0fc3fae9260",
+    "url": "/credit-card/static/media/chip.4ed75f15.png"
+  }
+]);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import CreditCardForm from './components/CreditCardForm'
-
-import './index.scss'
-
-ReactDOM.render(<CreditCardForm />, document.getElementById('root'))
